ce16aa6ba64dd850546315907d60260059a7ab0d
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-07-21 04:35:16 +0100

    Still learning

--- day1/index.js
+++ day1/index.js
@@ -1,20 +1,16 @@
-
+"use strict";
 /**
  * 1
  * This is lesson one to show how typesscript dont allow change of data type
  */
 // var sport = 'football';
 // var id = 5;
-
 // id = '5'; // Error: Type 'string' is not assignable to 
 //========================================================================
-
 let name = 'Danny';
 name.toLowerCase();
 console.log(name); // Danny - the string method didn't mutate the string
-
 let arr = [1, 3, 5, 7];
 arr.pop();
 console.log(arr); // [1, 3, 5] - the array method mutated the array
-
-name = 'Anna' // Assignment gives the primitive a new (not a mutated) value
+name = 'Anna'; // Assignment gives the primitive a new (not a mutated) value
